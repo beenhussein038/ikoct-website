1737ecbffb971aa04079c2c4ec24fa7950c2b8ae
Ada'illah Hussaini <beenhussein38@gmail.com>
2026-08-27 14:30:22 +0100

    Delete IKOCT/backend directory

--- IKOCT/backend/server/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-const config = require('../config/config');
-
-app.listen(config.port, () => {
-  console.log(`IKOCT backend API running on http://localhost:${config.port} (${config.env})`);
-  console.log(`Health check: http://localhost:${config.port}/api/health`);
-});
